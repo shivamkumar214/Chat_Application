f725cb9b078ac7321be057cd5fd4798ff4954d48
shivam <shivamkumar749362@gmail.com>
2026-05-05 12:54:30 +0530

    text size

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -88,7 +88,7 @@ const Sidebar = () => {
                         { ...prev, [user._id]:0 }
                     ))}}
                      key={idx} className={`relative flex items-center gap-2 p-2 pl-4 rounded
-                     cursor-pointer max-sm:text-sm ${selectedUser?._id === user._id && 'bg-[#4a1343]'}`}>
+                     cursor-pointer ${selectedUser?._id === user._id && 'bg-[#4a1343]'}`}>
 
                         <img src={user?.profilePic || assets.avatar_icon} alt="" className='w-9 aspect-auto  rounded-full'/>
 
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 
 
 // Middleware to protect routes
-export const protectRoute = async (req , res , next) => {
+export const   protectRoute = async (req , res , next) => {
     try{
         
         const token = req.headers.token;
